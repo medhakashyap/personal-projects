829cbca8afc195ae28cbeef3a33e93de5f06e6c8
Medha Kashyap <medha22292@iiitd.ac.in>
2023-12-23 14:27:05 +0530

    updated script.js

--- dictionary/script.js
+++ dictionary/script.js
@@ -5,22 +5,40 @@ const btn = document.getElementById("search-btn");
 
 btn.addEventListener("click", () => {
     let inpWord = document.getElementById("inp-word").value;
-    fetch(`${url}${inpWord}`).then((response) => response.json()).then((data) => {
-        console.log(data);
-        result.innerHTML = `<div class="word">
-        <h3>${inpWord}</h3>
-        <button><i class="fas fa-volume-up"></i></button>
-    </div>
-    <div class="details">
-        <p>pos</p>
-        <p>/sample/</p>
-    </div>
-    <div>
-        <p class="word-meaning">
-            Lorem ipsum adipisicing elit. Autem quod, magni, ipsam harum tempora nemo dolorem maiores.
-        </p>
-        <p class="word-example">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, ratione.</p>`;
-
-    });
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="word">
+                    <h3>${inpWord}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>`;
+            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+            sound.addEventListener("canplaythrough",playSound);
+        })
+        .catch(() => {
+            result.innerHTML = `<h2 class="error">Couldn't Find The Word</h2>`;
+        });
 });
-
+function playSound() {
+    try{
+        sound.play();
+    } 
+    catch(error){
+        console.error("Error playing sound: ",error);
+    }
+    sound.removeEventListener("canplaythrough",playSound);
+}
